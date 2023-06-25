fd0784de5c3119a2919cddb0e2548c9b088d65b8
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-06-25 13:25:19 -0300

    inicializando o projeto

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, } from 'react';
+import './App.css';
+
+function App() {
+  const [values, setValues] = useState();
+
+  const handleChangeValues = (values) => {
+    console.log(values)
+    setValues(prevValue => ({
+      ...prevValue,
+      [values.target]: values.target.values,
+    }));
+  };
+
+  return (
+    <div className='app-container'>
+      <div className='register--container'>
+        <h1 className='register--title'>Scrim Shop</h1>
+        <input type='text'
+          name='name'
+          placeholder='Nome'
+          className='register--input'
+          onChange={handleChangeValues}
+        />
+        <input type='text'
+          name='cost'
+          placeholder='Preço'
+          className='register--input'
+        />
+        <input type='text'
+          name='category'
+          placeholder='Categoria'
+          className='register--input'
+        />
+        <button className='register--button'>Cadastrar</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
